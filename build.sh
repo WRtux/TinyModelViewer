6390b16dba789c7858323e4d4b6623c36b5a53a0
Wilderness Ranger <wildernessrgr@aliyun.com>
2021-11-04 23:49:29 +0800

    Update build script
    Adjust and add static linking build..

--- build.sh
+++ build.sh
@@ -2,12 +2,24 @@ echo -n "Building project: "
 echo source/*.c
 gcc -Wall \
  -Os -fdata-sections -ffunction-sections -fno-asynchronous-unwind-tables \
+ -I include/ source/*.c \
  -mwindows -nostdlib -e start -Wl,--gc-sections -s \
- -I include/ source/*.c -L lib/ -l msvcrt -l kernel32 -l user32 -l gdiplus -l opengl32 -l glu32 -l glfw3dll \
+ -L lib/ -l opengl32 -l glu32 -l glfw3dll \
+ -l msvcrt -l kernel32 -l shell32 -l user32 -l gdi32 -l gdiplus \
  -o build/build-new.exe
+gcc \
+ -Os -fdata-sections -ffunction-sections -fno-asynchronous-unwind-tables \
+ -I include/ source/*.c \
+ -mwindows -nostartfiles -e start -Wl,--gc-sections -s \
+ -L lib/ -l opengl32 -l glu32 -l glfw3 -l gdiplus \
+ -o build/build-static.exe
 echo
-if test -x build/build-new.exe; then
+if test -a build/build-new.exe; then
  mv -f build/build-new.exe build/build.exe
+ if test -a build/build-static.exe; then
+  upx -9 build/build-static.exe
+  echo
+ fi
  tput setaf 2
  echo "Build complete. Running..."
  tput setaf 7
@@ -19,6 +31,7 @@ if test -x build/build-new.exe; then
  fi
  echo "Exited with ${result}."
 else
+ rm -f build/build-static.exe
  tput setaf 3
  echo "Build failed."
 fi
